a4eb43fd1519b9a6042bfc9f04a32867d4797c0b
Snowyz3 <bryanbongzihao@gmail.com>
2021-12-05 22:41:06 +0800

    5/12/2021

---
+++ libft/ft_putstr_fd.c
@@ -0,0 +1,20 @@
+#include "../includes/libft.h"
+
+void	ft_putstr_fd(char *s, int fd)
+{
+	size_t	i;
+
+	if (!s || !fd)
+		return ;
+	i = 0;
+	while (s[i])
+	{
+		ft_putchar_fd(s[i], fd);
+		i++;
+	}
+}
+/*
+int main()
+{
+	ft_putstr_fd("testr\n", 1);
+}*/
---
+++ libft/ft_strlen.c
@@ -0,0 +1,21 @@
+#include "../includes/libft.h"
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	len;
+
+	len = 0;
+	while (s[len])
+		len++;
+	return (len);
+}
+/*
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+	char *str = "abcde";
+
+	printf("%ld\n", ft_strlen(str));
+	printf("%ld\n", strlen(str));
+}*/
