237e5d2325aaf58d068dc4c64df250085f8c48e6
Snowyz3 <bryanbongzihao@gmail.com>
2021-12-07 19:31:46 +0800

    7/12/2021

--- srcs/ft_printf.c
+++ srcs/ft_printf.c
@@ -6,7 +6,15 @@ char	g_upperhex[16] = {'0', '1', '2', '3', '4', '5', '6', '7',
 '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
 int		g_len;
 
-static int	ft_putnbr(long long nbr)
+static void	ft_putnbr(long long nbr, char ch, int base)
+{
+	if (ch == 'X')
+		write(1, &g_upperhex[(nbr % base)], 1);
+	else
+		write(1, &g_lowerhex[(nbr % base)], 1);
+}
+
+static int	putnbr_base(long long nbr, char ch, int base)
 {
 	int	temp;
 
@@ -17,38 +25,16 @@ static int	ft_putnbr(long long nbr)
 		nbr *= -1;
 		ft_putchar_fd('-', 1);
 	}
-	if (nbr > 9)
+	if (nbr >= (long long)base)
 	{
-		ft_putnbr(nbr / 10);
-		ft_putchar_fd((nbr % 10) + 48, 1);
+		putnbr_base(nbr / base, ch, base);
+		ft_putnbr(nbr, ch, base);
 	}
 	else
-		ft_putchar_fd((nbr % 10) + 48, 1);
+		ft_putnbr(nbr, ch, base);
 	g_len++;
 	if (temp < 0)
 		return (g_len + 1);
-	return (g_len);
-}
-
-static int	putnbr_base(unsigned long long nbr, char ch, unsigned int base)
-{
-	g_len = 0;
-	if (nbr >= (unsigned long long)base)
-	{
-		putnbr_base(nbr / base, ch, base);
-		if (ch == 'X')
-			write(1, &g_upperhex[(nbr % base)], 1);
-		else
-			write(1, &g_lowerhex[(nbr % base)], 1);
-	}
-	else
-	{
-		if (ch == 'X')
-			write(1, &g_upperhex[(nbr % base)], 1);
-		else
-			write(1, &g_lowerhex[(nbr % base)], 1);
-	}
-	g_len++;
 	return (g_len);
 }
 
